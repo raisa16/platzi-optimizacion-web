ff710d085d543f2263acb486964a7e074680e13f
Raisa <git>
2025-06-24 22:27:23 -0700

    analisis y optimizacion del bundle con webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const webpack = require('webpack')
 const path = require('path')
+const shouldAnalyze = process.argv.includes('--analyze')
 
 const nodeEnv = process.env.NODE_ENV || 'development'
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const plugins = []
+if (shouldAnalyze){
+  plugins.push(new BundleAnalyzerPlugin())
+}
 const config = {
   mode: nodeEnv,
   entry: './src/index.js',
@@ -14,6 +21,7 @@ const config = {
   devServer: {
     static: '.',
   },
+  plugins,
 }
 
 module.exports = config
